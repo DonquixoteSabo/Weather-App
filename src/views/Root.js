3f8b50ef6bab2b464e4b0cbc3762af606c09be41
Donquixote Sabo <mixboksu@gmail.com>
2021-03-20 07:33:56 +0100

    Add setup for prettier, eslint and absolute imports

---
+++ src/views/Root.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const sayHello = a => {
+  console.log(a, 'siema');
+};
+
+sayHello('czesc');
+function Root() {
+  return (
+    <div>
+      <h1>siemka </h1>
+    </div>
+  );
+}
+
+export default Root;
