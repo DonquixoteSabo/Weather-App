16b0ad8be4ccd3b4b172d3d482eecc98d3f0d3e5
Donquixote Sabo <mixboksu@gmail.com>
2021-03-22 14:07:02 +0100

    Add better responsive styles

--- src/components/atoms/HeaderContainer/index.js
+++ src/components/atoms/HeaderContainer/index.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const HeaderContainer = styled.div`
   width: 100%;
+  height: 100vh;
   display: flex;
   justify-content: center;
   background-color: ${props => props.theme.colors.mirage};
