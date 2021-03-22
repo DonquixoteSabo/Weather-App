f3251dbc74f39023223f0b8ef1c090bd79bdf2cf
Donquixote Sabo <mixboksu@gmail.com>
2021-03-22 17:42:08 +0100

    Add media queries for mainTemplate

---
+++ src/components/atoms/BigContainer/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const BigContainer = styled.div`
+  flex-grow: 1;
+  background-color: ${props => props.theme.colors.vulcan};
+  min-height: 100%;
+`;
+
+export default BigContainer;
--- src/views/MainTemplate/index.js
+++ src/views/MainTemplate/index.js
@@ -1,21 +1,23 @@
 import { useState } from 'react';
 
+import BigContainer from 'components/atoms/BigContainer';
 import BigCart from 'components/organism/BigCart';
 import Search from 'components/organism/Search';
-
+import { Wrapper } from './styles';
 function MainTemplate() {
   const [isSearchActive, setIsSearchActive] = useState(false);
 
   const handleActiveChange = () => setIsSearchActive(prevState => !prevState);
 
   return (
-    <div>
+    <Wrapper>
       {isSearchActive ? (
         <Search handleActiveChange={handleActiveChange} />
       ) : (
         <BigCart handleActiveChange={handleActiveChange} />
       )}
-    </div>
+      <BigContainer>siema</BigContainer>
+    </Wrapper>
   );
 }
 
